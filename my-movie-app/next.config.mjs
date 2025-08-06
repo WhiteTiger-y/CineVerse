b22fe84fe560dbb36c214c7ae51711f78ee42be6
WhiteTiger-y <yashas.3319011621@ipu.ac.in>
2025-08-06 21:40:31 +0530

    preparing for render

--- my-movie-app/next.config.mjs
+++ my-movie-app/next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Add this line
+  output: 'export',
+};
 
-export default nextConfig;
+module.exports = nextConfig;
